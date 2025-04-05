53d6c1ff399d96849f273b3187623ee3ba14d69c
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-04-05 18:43:55 +0530

    Added code for created a new project on the backend

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,18 +1,29 @@
 import util from 'util';
 import child_process from 'child_process';
+import fs from 'fs/promises';
+import uuid4 from 'uuid4';
 
 const execPromisified = util.promisify(child_process.exec);
 
 export const createProjectController = async (req , res) => {
 
- const {stdout , stderr} = await execPromisified('pwd');
+ // create a unidue id and then inside the projects folder create a new folder with that id
+    const projectId = uuid4();
+    console.log("new projectid is :" , projectId);
+    
+    await fs.mkdir(`./projects/${projectId}`);
 
- console.log("stdout :" , stdout);
- console.log("stderr :" , stderr);
+ // after this call the npm vite@create command in the newly created projects folder
 
+    const response = await execPromisified('npm create vite@latest sandbox -- --template react' , {
+      cwd : `./projects/${projectId}`
+    })
+ 
+    
 
  return res.json({
   msg : "success",
+  data : projectId
  })
 
 
